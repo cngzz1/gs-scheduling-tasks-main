0a0bc7ccf625c89dd5f5cd849ee958c518acfedb
cngzz1 <67855012+cngzz1@users.noreply.github.com>
2022-05-09 16:13:53 +0800

    Commit initial version

---
+++ initial/src/test/java/com/example/schedulingtasks/SchedulingTasksApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.schedulingtasks;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SchedulingTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+		String actual = "";
+		assertEquals("", actual);
+	}
+
+}
